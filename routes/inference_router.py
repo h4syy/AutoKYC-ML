d946495c39610a692cce9dd186c420b5a7e28e06
Yash Paudel <yash.paudel@ime.com.np>
2024-10-28 11:17:12 +0545

    reset the system

--- routes/inference_router.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter, Depends, UploadFile, File
-from controllers.inference_controller import handle_inference
-
-router = APIRouter()
-
-@router.post("/run-inference")
-async def upload_file(file: UploadFile = File(...)):
-    print("log router")
-    return await handle_inference(file)
