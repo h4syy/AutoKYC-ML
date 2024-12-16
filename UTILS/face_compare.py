a865282e9d9b90a8c3ad10d8228c7629b419053a
Sulav Adhikari <sulav.adhikari@swifttech.com>
2024-12-16 15:39:25 +0545

    DONE DONE
    done with the flow-maintain
    
    now liveness test data left

---
+++ UTILS/face_compare.py
@@ -0,0 +1,118 @@
+from fastapi import APIRouter, UploadFile, HTTPException, File, Form
+from MODELS.schemas import FaceComparisonResponse, FaceComparisonResult
+from UTILS.aws_rekognition import run_face_comparison
+from UTILS.logger import logger
+import os
+import shutil
+import json
+from database import dbconfig
+from UTILS.image_cropper import image_cropper
+from UTILS.config import get_image_save_path
+
+router = APIRouter()
+
+@router.post("/face/compare", response_model=FaceComparisonResponse)
+async def face_compare_auto(
+    document_front: str = Form(...), 
+    liveness_document: str = Form(...), 
+    session_id: str = Form(...),
+    csid: str = Form(...),
+    msisdn: int = Form(...)
+):
+    logger.info("Face comparison inference started.")
+    
+    try:
+        document_front_path = document_front  
+        liveness_document_path = liveness_document  
+
+        result = await run_face_comparison(document_front_path, liveness_document_path)
+        source_image_details = result["source_image_bounding_box"]
+
+        cropped_image_path = None
+        if source_image_details:
+            cropped_image_path = get_image_save_path(msisdn, "cropped_image")
+            crop_result = image_cropper(document_front_path, source_image_details)
+            shutil.move(crop_result, cropped_image_path)
+            logger.info(f"Cropped image saved at: {cropped_image_path}")
+        else:
+            logger.warning("Source image bounding box not found in the result.")
+
+        face_matches = result['face_matches']
+        for match in face_matches:
+            similarity = match['similarity']
+            confidence = match['confidence']
+            details = {
+                "confidence": confidence,
+                "similarity": similarity,
+                "bounding_box": match['bounding_box'],
+            }
+
+        fc_status_decoded = await insert_face_compare_result(
+            session_id=session_id,
+            csid=csid,
+            Cropped_img_path=cropped_image_path,
+            confidence=confidence,
+            similarity=similarity,
+            details=details,
+            msisdn=msisdn,
+        )
+
+        if fc_status_decoded == 1:
+            payload = {
+                "ResponseData": {
+                    "IsDocumentScanCompleted": True,
+                    "IsVerified": True,
+                    "IsBackDocumentNeed": False,
+                    "DocumentType": 1,
+                },
+                "ResponseCode": 400,
+                "ResponseDescription": "Done"
+            }
+        else:
+            payload = {
+                "ResponseData": {
+                    "IsDocumentScanCompleted": False,
+                    "IsVerified": False,
+                    "IsBackDocumentNeed": True,
+                    "DocumentType": 1,
+                },
+                "ResponseCode": 400,
+                "ResponseDescription": "Redirect to Back"
+            }
+
+            logger.info("Document detection inference completed successfully.")
+            return payload
+
+        logger.info("Face comparison completed.")
+        return payload
+
+    except Exception as e:
+        logger.error(f"Error during face comparison: {e}")
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+async def insert_face_compare_result(session_id, csid, similarity, confidence, details, msisdn, Cropped_img_path):
+    sp_query = "CALL SP_INSERT_FACECOMPARE(%s, %s, %s, %s, %s, %s, %s)"
+    async with dbconfig.db_pool.acquire() as conn:
+        async with conn.cursor() as cursor:
+            await cursor.execute(
+                sp_query,
+                (msisdn, session_id, csid, confidence, similarity, Cropped_img_path, json.dumps(details)),
+            )
+
+            result = await cursor.fetchall()
+            if result:
+                row = result[0]
+                msg = row[0]
+                fc_status = row[1]
+                sp_code = row[2]
+
+                fc_status_decoded = int.from_bytes(fc_status, byteorder="big")
+                logger.info(f"Stored procedure response fc_status: {fc_status}")
+            else:
+                fc_status_decoded = None
+                logger.warning("No fc_status returned from the stored procedure")
+
+            await conn.commit()
+            logger.info("Detections inserted into database via stored procedure")
+            return fc_status_decoded
--- routers/document_detection_back.py
+++ routers/document_detection_back.py
@@ -2,48 +2,46 @@ from fastapi import APIRouter, UploadFile, File, HTTPException, Form
 from MODELS.schemas import Detection, DetectionResponse
 from UTILS.logger import logger
 from database import dbconfig
+from UTILS.face_compare import face_compare_auto
 import shutil
 import os
 import torch
 import json
-from UTILS.aws_rekognition import run_face_comparison
+from UTILS.config import get_image_save_path
+
 router = APIRouter() 
 
 model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt', force_reload=False)
 
-output_dir = '/output'
-os.makedirs(output_dir, exist_ok=True)
-
-@router.post("/document-detection/inference/back", response_model=DetectionResponse)
+@router.post("/document-detection/inference/back")
 async def detect_document(
     file: UploadFile = File(...),
     session_id: str = Form(...),
     csid: str = Form(...),
     msisdn: int = Form(...)
 ):
-    logger.info(f"Document detection inference started for session_id: {session_id}")
-    temp_image_path = os.path.join(output_dir, file.filename)
-        #face compare bata aako response fail bhayepachi yaha rakhne 
-    id_type_mapping = {
-        "CS": 1,   
-        "DL": 2, 
-        "NID": 3,  
-        "PP": 4   
-    }
-    
+    logger.info(f"Document detection inference started for msisdn_id: {msisdn}")
+    document_photo_path_back = get_image_save_path(msisdn, "Id_back")
     try:
-        with open(temp_image_path, "wb") as buffer:
+        with open(document_photo_path_back, "wb") as buffer:
             shutil.copyfileobj(file.file, buffer)
-        logger.info(f"File saved to {temp_image_path}")
+        logger.info(f"File saved to {document_photo_path_back}")
 
-        results = model(temp_image_path)
+        results = model(document_photo_path_back)
         if len(results.xyxy[0]) == 0:
             raise HTTPException(status_code=400, detail="No document detected.")
-        
-        detection_data = results.xyxy[0].tolist()[0]  
+
+        detection_data = results.xyxy[0].tolist()[0] 
         *box, confidence, cls = detection_data
         class_name = results.names[int(cls)]
-        suffix = class_name[-1] 
+
+        id_type_mapping = {
+            "CS": 1, 
+            "DL": 2,  
+            "NID": 3,  
+            "PP": 4   
+        }
+        suffix = class_name[-1]
         prefix = class_name[:-1] if suffix in {"F", "B"} else class_name
         id_type = id_type_mapping.get(prefix, -1)
 
@@ -52,7 +50,7 @@ async def detect_document(
             csid=csid,
             id_type=id_type,
             predicted_class=class_name,
-            document_photo_path=temp_image_path,
+            document_photo_path=document_photo_path_back,
             bounding_box=str(box),
             confidence=confidence,
             details={},
@@ -62,9 +60,26 @@ async def detect_document(
         dd_status_decoded = await insert_detections_into_db([detection])
 
         if dd_status_decoded == 1:
-        
-            result = await run_face_comparison("C:/Users/sulav.adhikari/Desktop/CHECK/shreshka.jpg", "C:/Users/sulav.adhikari/Desktop/CHECK/ImportedPhoto.755515784.737019.jpeg")
-            logger.info(f"Face Compare result:{result}")
+            async with dbconfig.db_pool.acquire() as conn:
+                async with conn.cursor() as cursor:
+                    await cursor.callproc('SP_FETCH_PHOTO_URL', (msisdn,))
+                    paths_result = await cursor.fetchall()
+
+                    if not paths_result or len(paths_result[0]) < 2:
+                        raise HTTPException(status_code=500, detail="Failed to fetch required document paths.")
+                    
+                    liveness_document_path, document_front_path = paths_result[0]
+
+                    print(document_front_path, liveness_document_path)
+
+            result = await face_compare_auto(
+                document_front=document_front_path,
+                liveness_document=liveness_document_path,
+                session_id=session_id,
+                csid=csid,
+                msisdn=msisdn
+            )
+            logger.info(f"Face Compare result: {result}")
 
             payload = {
                 "ResponseData": {
@@ -86,7 +101,7 @@ async def detect_document(
                     "IsBackDocumentNeed": True,
                     "DocumentType": id_type,
                 },
-                "ResponseCode": 400,
+                "ResponseCode": 469,
                 "ResponseDescription": "Redirect to Back"
             }
 
@@ -98,17 +113,14 @@ async def detect_document(
         raise HTTPException(status_code=500, detail=str(e))
     
     finally:
-        if os.path.exists(temp_image_path):
-            os.remove(temp_image_path)
-            logger.info(f"Temporary file {temp_image_path} removed.")
-
-            
+        if os.path.exists(document_photo_path_back):
+            logger.info(f"Saved{msisdn}_{document_photo_path_back}.")
 
 async def insert_detections_into_db(detections: list[Detection]):
     async with dbconfig.db_pool.acquire() as conn:
         async with conn.cursor() as cursor:
             for detection in detections:
-                # Execute the stored procedure
+   
                 await cursor.callproc('SP_INSERT_DD', (
                     detection.msisdn,
                     detection.session_id,
@@ -122,17 +134,15 @@ async def insert_detections_into_db(detections: list[Detection]):
                     1
                 ))
                 
-                    # Fetch the result set
                 result = await cursor.fetchall()
                 if result:
 
                     row = result[0] 
                     msg = row[0] 
                     dd_status = row[1]
-                    sp_code = row[2]  # Extract the first value (e.g., '1')
+                    sp_code = row[2] 
                     
                     dd_status_decoded =  int.from_bytes(dd_status, byteorder='big')
-
 
                     logger.info(f"Stored procedure response dd_status: {dd_status}")
                 else:
@@ -146,4 +156,4 @@ async def insert_detections_into_db(detections: list[Detection]):
 
             await conn.commit()
             logger.info("Detections inserted into the database via stored procedure.")
-            return dd_status_decoded  # Return the extracted status
+            return dd_status_decoded 
